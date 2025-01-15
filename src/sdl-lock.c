25bee5489fe8480e539e6c11a6d3cab1d5c19cbc
nyashbox <nyashbox@tutanota.com>
2025-01-15 22:11:52 +0200

    refactor: parse command line options

--- src/sdl-lock.c
+++ src/sdl-lock.c
@@ -5,10 +5,13 @@
 #include <stdlib.h>
 
 #include "core/log.h"
+#include "core/opts.h"
 
 int main(int argc, char **argv) {
   // add default stderr file sink for logging
   sdlk_logger_add_stderr_sink(&SDLK_DEFAULT_LOGGER);
+
+  sdlk_opts_parse(&SDLK_DEFAULT_CONFIG, argc, argv);
 
   // close all opened log files
   sdlk_logger_close_file_sinks(&SDLK_DEFAULT_LOGGER);

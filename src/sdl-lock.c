95235def19978741c44992432e55fabccce61dce
nyashbox <nyashbox@tutanota.com>
2025-01-15 22:10:38 +0200

    refactor: add logger initialization and sinks clean up

--- src/sdl-lock.c
+++ src/sdl-lock.c
@@ -4,4 +4,14 @@
 
 #include <stdlib.h>
 
-int main(int argc, char **argv) { return EXIT_SUCCESS; }
+#include "core/log.h"
+
+int main(int argc, char **argv) {
+  // add default stderr file sink for logging
+  sdlk_logger_add_stderr_sink(&SDLK_DEFAULT_LOGGER);
+
+  // close all opened log files
+  sdlk_logger_close_file_sinks(&SDLK_DEFAULT_LOGGER);
+
+  return EXIT_SUCCESS;
+}

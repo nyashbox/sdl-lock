e7b350d25715d9dfa6237e130d4963728ec10fea
nyashbox <nyashbox@tutanota.com>
2025-01-15 23:39:28 +0200

    feat(core/opts): set logger level from command line

--- src/core/opts.c
+++ src/core/opts.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (c) 2025 nyashbox and Contributors
 
+#include "core/status.h"
 #include <getopt.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -16,17 +17,20 @@
 // long options
 static struct option LONGOPTS[] = {{"version", no_argument, NULL, 'v'},
                                    {"help", no_argument, NULL, 'h'},
+                                   {"log-level", required_argument, NULL, 'L'},
                                    {NULL, no_argument, NULL, 0}};
 
 // help message
 static const char HELP_MSG[] =
     "Usage: sdl-lock [OPTION...]\n\n"
     "Options:\n"
+    "\t-L, --log-level LEVEL Logging level (default: warn)\n"
+    "\n"
     "\t-h, --help    Display this help message and exit.\n"
     "\t-v, --version Display version and exit.\n";
 
 void sdlk_opts_parse(sdlk_config_t *conf, int argc, char **argv) {
-  const char *shortopts = "vh";
+  const char *shortopts = "vhL:";
 
   int o;
   int longindex = 0;
@@ -36,6 +40,25 @@ void sdlk_opts_parse(sdlk_config_t *conf, int argc, char **argv) {
       printf("sdl-lock version %s\n", SDL_LOCK_VERSION);
 
       exit(EXIT_SUCCESS);
+    }
+    case 'L': {
+      const char *level = optarg;
+
+      sdlk_status_t res;
+
+      sdlk_logger_level_t lvl = sdlk_logger_level_from_str(level, &res);
+      if (res != SDLK_STATUS_SUCCESS) {
+        SDLK_LOGGER_FATAL("(Core/Opts) Failed to parse logger level: %s\n",
+                          level);
+
+        exit(EXIT_FAILURE);
+      }
+
+      // set default logger level
+      sdlk_logger_set_level(&SDLK_DEFAULT_LOGGER, lvl);
+      SDLK_LOGGER_DEBUG("(Core/Opts) Setting log level to %s!\n", level);
+
+      break;
     }
     case 'h':
       /* fallthrough */
